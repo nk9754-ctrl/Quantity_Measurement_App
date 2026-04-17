ec9bcdc15000220f3e63e7e9a0be5345ee007922
nk9754-ctrl <nikhilsaikandru6@gmail.com>
2026-04-17 10:58:37 +0530

    UC3: Generic Quantity Class for DRY Principle

--- src/Quantity_Measurement_App.java
+++ src/Quantity_Measurement_App.java
@@ -1,51 +1,48 @@
 public class Quantity_Measurement_App {
 
-    public static class Feet {
-        private final double value;
+    enum LengthUnit {
+        FEET(1.0),
+        INCH(1.0 / 12.0);
 
-        public Feet(double value) {
+        private final double factor;
+
+        LengthUnit(double factor) {
+            this.factor = factor;
+        }
+
+        public double toFeet(double value) {
+            return value * factor;
+        }
+    }
+
+    static class QuantityLength {
+        double value;
+        LengthUnit unit;
+
+        public QuantityLength(double value, LengthUnit unit) {
+            if (unit == null) throw new IllegalArgumentException();
             this.value = value;
+            this.unit = unit;
+        }
+
+        double toFeet() {
+            return unit.toFeet(value);
         }
 
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
             if (obj == null || getClass() != obj.getClass()) return false;
-            Feet feet = (Feet) obj;
-            return Double.compare(feet.value, value) == 0;
+
+            QuantityLength other = (QuantityLength) obj;
+            return Double.compare(this.toFeet(), other.toFeet()) == 0;
         }
-    }
-
-    public static class Inches {
-        private final double value;
-
-        public Inches(double value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-            Inches inches = (Inches) obj;
-            return Double.compare(inches.value, value) == 0;
-        }
-    }
-
-    public static boolean compareFeet(double v1, double v2) {
-        Feet f1 = new Feet(v1);
-        Feet f2 = new Feet(v2);
-        return f1.equals(f2);
-    }
-
-    public static boolean compareInches(double v1, double v2) {
-        Inches i1 = new Inches(v1);
-        Inches i2 = new Inches(v2);
-        return i1.equals(i2);
     }
 
     public static void main(String[] args) {
-        System.out.println(compareFeet(1.0, 1.0));
-        System.out.println(compareInches(1.0, 1.0));
+        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
+        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+
+        System.out.println(q1.equals(q2));
     }
 }
--- test/Quantity_Measurement_AppTest.java
+++ test/Quantity_Measurement_AppTest.java
@@ -4,35 +4,59 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Quantity_Measurement_AppTest {
 
     @Test
-    void testEquality_SameValue() {
-        Quantity_Measurement_App.Feet f1 = new Quantity_Measurement_App.Feet(1.0);
-        Quantity_Measurement_App.Feet f2 = new Quantity_Measurement_App.Feet(1.0);
-        assertTrue(f1.equals(f2));
+    void testFeetToFeet() {
+        Quantity_Measurement_App.QuantityLength q1 =
+                new Quantity_Measurement_App.QuantityLength(1.0,
+                        Quantity_Measurement_App.LengthUnit.FEET);
+
+        Quantity_Measurement_App.QuantityLength q2 =
+                new Quantity_Measurement_App.QuantityLength(1.0,
+                        Quantity_Measurement_App.LengthUnit.FEET);
+
+        assertEquals(q1, q2);
     }
 
     @Test
-    void testEquality_DifferentValue() {
-        Quantity_Measurement_App.Feet f1 = new Quantity_Measurement_App.Feet(1.0);
-        Quantity_Measurement_App.Feet f2 = new Quantity_Measurement_App.Feet(2.0);
-        assertFalse(f1.equals(f2));
+    void testFeetToInch() {
+        Quantity_Measurement_App.QuantityLength q1 =
+                new Quantity_Measurement_App.QuantityLength(1.0,
+                        Quantity_Measurement_App.LengthUnit.FEET);
+
+        Quantity_Measurement_App.QuantityLength q2 =
+                new Quantity_Measurement_App.QuantityLength(12.0,
+                        Quantity_Measurement_App.LengthUnit.INCH);
+
+        assertEquals(q1, q2);
     }
 
     @Test
-    void testEquality_NullComparison() {
-        Quantity_Measurement_App.Feet f1 = new Quantity_Measurement_App.Feet(1.0);
-        assertFalse(f1.equals(null));
+    void testDifferentValues() {
+        Quantity_Measurement_App.QuantityLength q1 =
+                new Quantity_Measurement_App.QuantityLength(1.0,
+                        Quantity_Measurement_App.LengthUnit.FEET);
+
+        Quantity_Measurement_App.QuantityLength q2 =
+                new Quantity_Measurement_App.QuantityLength(2.0,
+                        Quantity_Measurement_App.LengthUnit.FEET);
+
+        assertNotEquals(q1, q2);
     }
 
     @Test
-    void testEquality_NonNumericInput() {
-        Quantity_Measurement_App.Feet f1 = new Quantity_Measurement_App.Feet(1.0);
-        String obj = "test";
-        assertFalse(f1.equals(obj));
+    void testNullComparison() {
+        Quantity_Measurement_App.QuantityLength q1 =
+                new Quantity_Measurement_App.QuantityLength(1.0,
+                        Quantity_Measurement_App.LengthUnit.FEET);
+
+        assertFalse(q1.equals(null));
     }
 
     @Test
-    void testEquality_SameReference() {
-        Quantity_Measurement_App.Feet f1 = new Quantity_Measurement_App.Feet(1.0);
-        assertTrue(f1.equals(f1));
+    void testDifferentType() {
+        Quantity_Measurement_App.QuantityLength q1 =
+                new Quantity_Measurement_App.QuantityLength(1.0,
+                        Quantity_Measurement_App.LengthUnit.FEET);
+
+        assertFalse(q1.equals("test"));
     }
 }
